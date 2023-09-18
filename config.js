aac34dc1e3e8f90f23716a536b92ad7dfee3439e
CafeMix <145397340+CafeMix@users.noreply.github.com>
2023-09-18 16:43:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxDsp61qa7t6Wm5y0G0SkkFBVFGdW9psgyc99x8fkI_7b9pAM-nGPrhFNxfe8DmnJkL/exec";
